1fd456183860abc7c359573a5af8a5364fecf356
Cyber_User <ad6354@bs.amalnet.k12.il>
2024-04-15 19:33:33 +0300

    finished but there is an issue that once I press enter for the first time, it wont show me the answer but 'NaN' instead

--- app/src/main/java/com/example/proj13/MainActivity.java
+++ app/src/main/java/com/example/proj13/MainActivity.java
@@ -2,6 +2,7 @@ package com.example.proj13;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
@@ -15,9 +16,9 @@ public class MainActivity extends AppCompatActivity
 {
     TextView tvx1, tvx2;
     EditText edA, edB, edC;
-    ImageView func;
     double a, b, c, ans1,ans2;
     String inputA,inputB,inputC;
+    private static final int secretCode = 123;
 
     //----------------------------------------------------------------
 
@@ -28,10 +29,30 @@ public class MainActivity extends AppCompatActivity
         setContentView(R.layout.activity_main);
         tvx1 = (TextView) findViewById(R.id.textView4);
         tvx2 = (TextView) findViewById(R.id.textView5);
+        tvx1.setVisibility(View.INVISIBLE);
+        tvx2.setVisibility(View.INVISIBLE);
         edA = (EditText) findViewById(R.id.editTextNumberDecimal);
         edB = (EditText) findViewById(R.id.editTextNumberDecimal2);
         edC = (EditText) findViewById(R.id.editTextNumberDecimal3);
-        func = (ImageView) findViewById(R.id.imageView);
+    }
+    protected void onActivityResult(int source, int result, Intent data)
+    {
+        super.onActivityResult(source, result, data);
+        if (source == secretCode)
+        {
+            if (result == Activity.RESULT_OK)
+            {
+                if (data != null)
+                {
+                    ans1 = data.getDoubleExtra("x1",0);
+                    ans2 = data.getDoubleExtra("x2",0);
+                    tvx1.setVisibility(View.VISIBLE);
+                    tvx2.setVisibility(View.VISIBLE);
+                    tvx1.setText("X1 =" + ans1);
+                    tvx2.setText("X2 =" + ans2);
+                }
+            }
+        }
     }
 
     //----------------------------------------------------------------
@@ -76,21 +97,22 @@ public class MainActivity extends AppCompatActivity
 
 
 
-    public void moveToSecView(View view)
-    {
+    public void moveToSecView(View view) {
         inputA = edA.getText().toString();
         inputB = edB.getText().toString();
         inputC = edC.getText().toString();
-        if ((valid(inputA)) && (valid(inputB)) && (valid(inputC)))
-        {
-            a = Integer.parseInt(inputA);
-            b = Integer.parseInt(inputB);
-            c = Integer.parseInt(inputC);
+        if ((valid(inputA)) && (valid(inputB)) && (valid(inputC))) {
+            a = Double.parseDouble(inputA);
+            b = Double.parseDouble(inputB);
+            c = Double.parseDouble(inputC);
             Intent warp = new Intent(this, theMathFormulaCalculator.class);
             warp.putExtra("a value", a);
             warp.putExtra("b value", b);
             warp.putExtra("c value", c);
-            startActivity(warp);
+            startActivityForResult(warp, secretCode);
         }
     }
+
+
+
 }
--- app/src/main/java/com/example/proj13/theMathFormulaCalculator.java
+++ app/src/main/java/com/example/proj13/theMathFormulaCalculator.java
@@ -2,29 +2,87 @@ package com.example.proj13;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.annotation.SuppressLint;
 import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.view.View;
+import android.webkit.WebView;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 public class theMathFormulaCalculator extends AppCompatActivity
 {
-    Intent get = getIntent();
-    int a,b,c;
-    double X1,X2;
-    TextView ans1,ans2;
+    double a, b, c;
+    double X1, X2;
+    TextView ans1, ans2;
+    ImageView showFunc;
     @Override
     protected void onCreate(Bundle savedInstanceState)
     {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_the_math_formula_calculator);
-        ans1 = (TextView) findViewById(R.id.textView6);
-        ans2 = (TextView) findViewById(R.id.textView7);
-        a = get.getIntExtra("a value", 1);
-        b = get.getIntExtra("b value", 1);
-        c = get.getIntExtra("c value", 1);
-        X1 = (-b + Math.sqrt(b*b - 4 * a *b))/(2*a);
-        X2 = (-b - Math.sqrt(b*b - 4 * a *b))/(2*a);
-        ans1.setText(String.valueOf(X1));
-        ans2.setText(String.valueOf(X2));
+
+        ans1 = findViewById(R.id.textView6);
+        ans2 = findViewById(R.id.textView7);
+        showFunc =findViewById(R.id.imageView2);
+        showFunc.setImageResource(R.drawable.ic_launcher_background);
+
+        Intent secPhase = getIntent();
+        a = secPhase.getDoubleExtra("a value", 0);
+        b = secPhase.getDoubleExtra("b value", 0);
+        c = secPhase.getDoubleExtra("c value", 1);
+
+        X1 = (-b + Math.sqrt(b * b - 4 * a * c)) / (2 * a);
+        X2 = (-b - Math.sqrt(b * b - 4 * a * c)) / (2 * a);
+
+        ans1.setText("ans1 =" + X1);
+        ans2.setText("ans2 =" + X2);
+
+        secPhase.putExtra("x1",X1);
+        secPhase.putExtra("x2",X2);
+        setResult(RESULT_OK, secPhase);
+        ShowFuncPic(a, b, c);
+    }
+
+    private void ShowFuncPic(double a, double b, double c)
+    {
+        if (a > 0)
+        {
+            if (b > 0)
+            {
+                showFunc.setImageResource(R.drawable.func4);
+            }
+            else if (b < 0)
+            {
+                showFunc.setImageResource(R.drawable.func5);
+            }
+            else
+            {
+                showFunc.setImageResource(R.drawable.func1);
+            }
+        }
+        if (a < 0)
+        {
+            if (b > 0)
+            {
+                showFunc.setImageResource(R.drawable.func3);
+            }
+            else if (b < 0)
+            {
+                showFunc.setImageResource(R.drawable.func6);
+            }
+            else
+            {
+                showFunc.setImageResource(R.drawable.func2);
+            }
+        }
+    }
+
+    public void returned(View view)
+    {
+
+        finish();
+
     }
 }
